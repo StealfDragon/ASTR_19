bc0d75a13595bb36686ec171a77d75d98525eeff
Cassian Jones <cassian.jones@icloud.com>
2025-09-30 12:45:58 -0700

    first commit

---
+++ Week1/Session1/session1.py
@@ -0,0 +1,4 @@
+print("Name: Cassian Jones\n" \
+"Pronouns: he/him\n" \
+"Favorite movie: Pulp Fiction, it was the most unique movie-watching experience I've ever had.\n" \
+"Favorite food: Ribeye steak and potatoes au gratin, the way the flavors and textures come together is incredible")
