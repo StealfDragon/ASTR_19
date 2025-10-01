0f96556e485aa69eab558fff075c6e0789df66cf
Cassian Jones <cassian.jones@icloud.com>
2025-09-30 17:32:40 -0700

    session 5 initial commit

---
+++ Week3/Session5/session5.py
@@ -0,0 +1,25 @@
+import math
+
+def main():
+    x = 0.0
+    print("-----------------------------")
+    print("|             |             |")
+    print("|      x      |    sin(x)   |")
+    print("|             |             |")
+    print("-----------------------------")
+    while(x <= (2 * math.pi)):
+        print("| %-11f | %-11f |" % (x, math.sin(x)))
+        print("-----------------------------")
+        x += (2 * math.pi / 1000)
+    
+if __name__ == '__main__':
+    main()
+
+
+"""
+-----------------------------
+|             |             |
+-----------------------------
+for x in range(0, 2 * math.pi, (2 * math.pi) / 1000):
+for 0, (2 * pi) in enumerate((2 * math.pi) / 1000):
+"""
