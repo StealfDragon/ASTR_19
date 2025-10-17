08091e538a1f15b57083de29f9f3c84f5cd682d9
Cassian Jones <cassian.jones@icloud.com>
2025-10-17 15:40:22 -0700

    Marginally improved session 4, added markdown cells to session 6

--- Week2/Session4/session4.py
+++ Week2/Session4/session4.py
@@ -8,16 +8,6 @@ class Animal:
         self.hasFur = hasFur
 
     def __str__(self):
-        """ print(f"A {self.name}'s arms are {self.lenArms} inches long.")
-        print(f"A {self.name}'s legs are {self.lenLegs} inches long.")
-        print(f"A {self.name} has {self.numEyes} eyes.")
-        print(f"A {self.name} ")
-        print("has") if (self.hasTail) else print("does not have")
-        print(" a tail.")
-        print(f"A {self.name} ")
-        print("has") if (self.hasFur) else print("does not have")
-        print(" fur.") """
-
         tailStr = f"A {self.name} has a tail." if self.hasTail else f"A {self.name} does not have a tail."
         furStr = f"A {self.name} has fur." if self.hasTail else f"A {self.name} does not have fur."
 
@@ -29,3 +19,6 @@ A {self.name} has {self.numEyes} eyes."
 
 snake = Animal("snake", 0, 0, 2, True, False)
 print(snake)
+
+lion = Animal("lion", 0, 0, 2, True, False)
+print(lion)
---
+++ test.py
@@ -0,0 +1,7 @@
+import numpy as np
+
+x = np.linspace(0.0, 1.0, 9)
+
+print(x)
+print("")
+print(x**2)
